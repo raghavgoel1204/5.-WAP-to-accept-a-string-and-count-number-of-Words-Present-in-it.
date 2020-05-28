934426e89c060a0926f46676a6023d379ff2a8c6
USER <USER@Devansh>
2020-05-28 22:16:25 +0530

    1.	WAP to accept a String from User and Display it Back on Screen

---
+++ src/p1.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+//1.	WAP to accept a String from User and Display it Back on Screen    
+public class p1 
+
+{
+        public static void main(String [] args)
+    {
+    
+        Scanner sc =new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println(name);
+    }
+    
+}
